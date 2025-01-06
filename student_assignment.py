7b8e75d9122f26c893cd7b0d1e4b53208b564919
JeffreyCC <jeffrey_cc_chen@asus.com>
2025-01-06 16:32:54 +0800

    Remove \n

--- student_assignment.py
+++ student_assignment.py
@@ -40,7 +40,7 @@ def generate_hw01(question):
 
     response = llm.invoke(all_msgs)
 
-    return response.content
+    return response.content.replace("\n", "")
     
 def generate_hw02(question):
     pass
